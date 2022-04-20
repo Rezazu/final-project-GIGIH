fbfcc9d61ed68af4882e60298401fb8fab51c4e2
Rezazu <rezky126>
2022-04-20 05:42:25 +0000

    Import Boostrap and Javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
   resources :foods
 
-  root to: "foods#index", as: "foods_index"
+  #root
+  root to: "foods#index", 
+    as: "foods_index"
 
   # get "/items/new", to: "items#new", as: "items_new"
   # post "/items/new", to: "items#create", as: "items_create"
-
-  get "/:id", to: "foods#show", as: "foods_show"
   
-  get "/:id/edit", to: "foods#edit", as: "foods_edit"
-  
-  patch "/:id/update", to: "foods#update", as: "foods_update"
-  
-  delete "/foods/:id/delete", to: "foods#destroy", as: "foods_delete"
+  #Show food by params
+  get "/:id",
+    to: "foods#show",
+    as: "foods_show"
+  #edit food by params
+  get "/:id/edit", 
+    to: "foods#edit", 
+    as: "foods_edit"
+  #update food by params
+  patch "/:id/update", 
+    to: "foods#update", 
+    as: "foods_update"
+  #delete food by params
+  delete "/foods/:id/delete", 
+    to: "foods#destroy", as: 
+    "foods_delete"
 end
