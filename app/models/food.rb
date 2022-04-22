9dcc85afc3bad757f7a920e100cc6a4b948bc723
Rezazu <rezky126>
2022-04-22 13:07:46 +0000

    Add validation description, price on Food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,13 @@
 class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: {maximum: 150}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
 
+  has_many :categories, through: :menu_categories
+  has_many :orders
+  has_many :orders, through: :menu_orders
+
+  
   def self.by_letter(letter)
     where("name LIKE ?", "#{letter}%").order(:name)
   end
